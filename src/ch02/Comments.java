262ece652ae833bb62d4e396dec2bc05253fcb88
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-03-09 14:43:25 +0300

    Proje Eklendi

---
+++ src/ch02/Comments.java
@@ -0,0 +1,23 @@
+package ch02;
+
+/**
+ * @author dogukan
+ */
+public class Comments {
+
+    private String name = "Comments"; // That's just a String.
+
+    /**
+     * This method calculates the double of passed parameter and returns it.
+     *
+     * @param x		The value to be doubled.
+     * @return 		Double of the value of x passed as parameter.
+     */
+    public int doubleIt(int x) {
+
+        /*
+         * That's a comment block!
+         */
+        return 2 * x;
+    }
+}
