04a083e37210ce841c2abea03a429b5f99c34443
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2022-03-23 22:58:58 +0300

    Course 10 some changes applies.

--- src/ch10/packaging/staticImport/AClass.java
+++ src/ch10/packaging/staticImport/AClass.java
@@ -1,14 +1,6 @@
 
-package org.javaturk.oopj.ch10.packaging.staticImport;
+package ch10.packaging.staticImport;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         For more info please
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>
- */
 public class AClass {
 
 	public static boolean aStaticVariable;
--- src/ch10/packaging/staticImport/StaticImport.java
+++ src/ch10/packaging/staticImport/StaticImport.java
@@ -1,25 +1,18 @@
 
-package org.javaturk.oopj.ch10.packaging.staticImport;
+package ch10.packaging.staticImport;
 
 //import static java.lang.Math.pow;
 //import static java.lang.Math.PI;
 
 import static java.lang.Math.*;
 
-import static org.javaturk.oopj.ch10.packaging.staticImport.AClass.*;
+import static ch10.packaging.staticImport.AClass.*;
 
 //import static org.javaturk.oopj.ch10.packaging.staticImport.AClass.A_CONSTANT;
 //import static org.javaturk.oopj.ch10.packaging.staticImport.AClass.aStaticVariable;
 //import static org.javaturk.oopj.ch10.packaging.staticImport.AClass.aStaticMethod;
 
-/**
- * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
- *         <p>
- *         For more info please
- * @see <a href="http://www.javaturk.org">http://www.javaturk.org</a>
- * @see <a href="http://www.selsoft.com.tr">http://www.selsoft.com.tr</a>
- *      </p>
- */
+
 public class StaticImport {
 
 	public static void main(String[] args) {
